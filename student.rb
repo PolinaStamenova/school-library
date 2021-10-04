ee089502af7653bd673f575ea26e520f4dd8fa40
PolinaStamenova <polina.stamenova@gmail.com>
2021-10-04 17:01:34 +0300

    Rename file main to person

--- student.rb
+++ student.rb
@@ -1,4 +1,4 @@
-require_relative 'main'
+require_relative 'person'
 
 class Student < Person
   attr_reader :classroom
--- teacher.rb
+++ teacher.rb
@@ -1,4 +1,4 @@
-require_relative 'main'
+require_relative 'person'
 
 class Teacher < Person
   def initialize(params)
