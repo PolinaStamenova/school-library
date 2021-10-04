d916d1cdea81c2c6d9f2f0af1ca544675d640149
PolinaStamenova <polina.stamenova@gmail.com>
2021-10-04 16:41:50 +0300

    Create Teacher and Student subclasses of Person

--- main.rb
+++ main.rb
@@ -1,56 +1,21 @@
 class Person
-  def initialize(age, name = 'Unknown', parent_permission: true)
+  attr_reader :id
+  attr_accessor :name, :age
+
+  def initialize(params)
     @id = Random.rand(1..1000)
-    @name = name
-    @age = age
-    @parent_permission = parent_permission
+    @name = params[:name] || 'Unknown'
+    @age = params[:age]
+    @parent_permission = params.fetch(:parent_permission, true)
   end
 
-  # attr_accessor :id
-  # attr_accessor :name
-  # attr_accessor :age
-
-  # Attributes readers/setters  => attr_reader :name
-  def id
-    @id = id
-  end
-
-  def name
-    @name = name
-  end
-
-  def age
-    @age = age
-  end
-
- # Attributes writers/getters  => attr_writer :name
-  def id = (id)
-    @id = id
-  end
-
-  def name = (name)
-    @name = name
-  end
-
-  def age = (age)
-    @age = age
-  end
-
-  #Public method
   def can_use_services?
-    if @age >= 18 || @parent_permission === true
-      true
-    else false
-    end
+    of_age? || @parent_permission
   end
 
-  #Private method
-  def is_of_age? (age)
-    @age = age
-    if @age >= 18
-      true
-    else
-      false
-    end
+  private
+
+  def of_age?
+    age >= 18
   end
 end
