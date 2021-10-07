8c38a222c3586ef68d39a2b9f9d26bfe5aa573df
PolinaStamenova <polina.stamenova@gmail.com>
2021-10-07 13:56:08 +0300

    Create option_three method that create student or teache instance of class Person and store it into initialized variable @people

--- main.rb
+++ main.rb
@@ -1,23 +1,21 @@
 require_relative "person"
 require_relative "student"
 require_relative "teacher"
-
+require_relative "corrector"
 
 
 class App
   def initialize
-    @classroom = Classroom.new('Microverse 2.0')
+    # @classroom = Classroom.new('Microverse 2.0')
     @people = []
-    @books = []
-    @rentals = []
+    # @books = []
+    # @rentals = []
   end
 
   def run
     puts "Welcome to School Library App!"
-    puts
-    print options
-    option = gets.chomp
-    case_options(option)
+    puts options
+
   end
 
   def case_options(option)
@@ -27,7 +25,7 @@ class App
     when "2"
       puts "Two"
     when "3"
-      puts "Three"
+      option_three
     when "4"
       puts "four"
     when "5"
@@ -39,7 +37,37 @@ class App
     end
   end
 
+  def option_three
+    print "Do you want to create a student (1) or a teacher (2)? [Input the number]: "
+    choose_person = gets.chomp
+    print "Age: "
+    age = gets.chomp.to_i
+    print "Name: "
+    name = gets.chomp
+    if choose_person == "1"
+      print "Has parent permission? [Y/N]: "
+      parent_permission = gets.chomp.downcase
+      if parent_permission == "y"
+        parent_permission = true
+      elsif parent_permission == "n"
+        parent_permission = false
+      end
+      student = Student.new(age: age, name: name, parent_permission: parent_permission, corrector: Corrector.new)
+      @people << student
+      # p @people
+    elsif choose_person == "2"
+      print "Specialization: "
+      specialization = gets.chomp
+      teacher = Teacher.new(age: age, name: name, specialization: specialization, corrector: Corrector.new)
+      @people << teacher
+      # p @people
+    end
+    puts "Person created successfully"
+    puts options
+  end
+
   def options
+    puts
     puts "Please choose an option by enterin a number:"
     puts "1 - List all books"
     puts "2 - List all people"
@@ -48,6 +76,8 @@ class App
     puts "5 - Create a rental"
     puts "6 - List all rentals for a given preson id"
     puts "7 - Exit"
+    option = gets.chomp
+    case_options(option)
   end
 end
 
