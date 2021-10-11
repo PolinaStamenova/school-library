ce12556adcbdfbe3413012f4b88ee7f4c21b9a86
PolinaStamenova <polina.stamenova@gmail.com>
2021-10-11 16:44:56 +0300

    Implemnt stringify method in storage class

--- storage.rb
+++ storage.rb
@@ -5,5 +5,9 @@ require_relative 'teacher'
 require_relative 'rental'
 
 class Storage
-  def stringify; end
+  def stringify(book, people, rentals)
+    File.open('books.json', 'w') { |f| f.write JSON.generate(book) }
+    File.open('people.json', 'w') { |f| f.write JSON.generate(people) }
+    File.open('rentals.json', 'w') { |f| f.write JSON.generate(rentals) }
+  end
 end
