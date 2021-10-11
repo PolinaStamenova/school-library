dc3dca33408962e13b0e3ff63da5927e212d2224
olawale-o <omoogunolawale@gmail.com>
2021-10-11 14:08:59 +0100

    Define to_json method in Teacher class

--- teacher.rb
+++ teacher.rb
@@ -9,4 +9,14 @@ class Teacher < Person
   def can_use_services?
     true
   end
+
+  def to_json(*args)
+    {
+      JSON.create_id => self.class.name,
+      'id' => @id,
+      'age' => @age,
+      'specialization' => @specialization,
+      'parent_permission' => @parent_permission
+    }.to_json(*args)
+  end
 end
